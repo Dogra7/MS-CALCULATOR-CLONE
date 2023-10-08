f3e07813ad3cc049aa7e1bbd699df454e25c7388
Dogra7 <131335630+Dogra7@users.noreply.github.com>
2023-10-08 15:10:59 +0530

    3

--- try.js
+++ try.js
@@ -6,6 +6,26 @@ function display(value) {
 
 function clearDis(){
     document.getElementById("display").value = '';
+    document.getElementById("input").value = '';
 }
 
+function clearRes(){
+    document.getElementById("input").value = '';
+}
+
+function erase() {
+    var inputElement = document.getElementById("display");
+    var inputValue = inputElement.value;
+
+    if (inputValue.length > 0) {
+        inputValue = inputValue.slice(0, -1);
+        inputElement.value = inputValue;
+    }
+}
+
+function calculate() {
+    var x = document.getElementById("display").value;
+    var result = eval(x);
+    document.getElementById("input").value = ('= ')+result ;
+}
 
